d5f3770e8b95baab77a648d812a998e6f383dc67
Yoga Yudistira <yudistirayoga97@gmail.com>
2026-01-29 15:50:40 +0700

    Fix container rename to capture ID before zenity steals focus

--- bin/.local/bin/i3-rename-container.sh
+++ bin/.local/bin/i3-rename-container.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+# Capture focused container ID before zenity steals focus
+CON_ID=$(i3-msg -t get_tree | jq -r '.. | objects | select(.focused==true) | .id')
+
 NEW=$(zenity --text="Enter container name:" --entry --title="Rename container")
 if [ -n "$NEW" ]; then
-    i3-msg "title_format \"$NEW\""
+    i3-msg "[con_id=\"$CON_ID\"] title_format \"$NEW\""
 fi
